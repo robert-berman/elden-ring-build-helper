d1115540b4771c38b408ef8a61b255b4c7059bdd
Robert Berman <robertberman.tech@gmail.com>
2022-03-11 15:56:09 -0500

    created character and weapon db models

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,21 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
 
-const User = require('./models/User')
+const User = require('./models/User');
+const Weapons = require('./models/Weapons');
 
+const Character = require('./models/Character');
 //associations could go here!
+
+Character.belongsTo(User);
+User.hasMany(Character);
 
 module.exports = {
   db,
   models: {
     User,
+    Weapons,
+    Character,
   },
-}
+};
