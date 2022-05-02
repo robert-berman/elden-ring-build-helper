ae609cb46e542cf0904235b9c36dabf36c3d01ec
Robert Berman <robertberman.tech@gmail.com>
2022-05-02 14:50:16 -0400

    added functional searchbar

--- client/components/AllWeapons.js
+++ client/components/AllWeapons.js
@@ -1,57 +1,88 @@
-import React, { useEffect, useState } from 'react';
-import { getWeapons } from '../store/weapons';
-import { useHistory } from 'react-router-dom';
-import { fetchSingleWeapon } from '../store/weapons';
-import { Link } from 'react-router-dom';
-import { useStyles } from './AllWeaponStyles';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { getWeapons } from "../store/weapons";
+import { useHistory } from "react-router-dom";
+import { fetchSingleWeapon } from "../store/weapons";
+import { Link } from "react-router-dom";
+import { useStyles } from "./AllWeaponStyles";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "@material-ui/core";
 
 const AllWeapons = () => {
   const dispatch = useDispatch();
   const classes = useStyles();
   const weapons = useSelector((state) => state.weapons.weapons);
   const history = useHistory();
+  const [search, setSearch] = useState("");
 
   const handleClick = (id) => {
     dispatch(fetchSingleWeapon(id));
     history.push(`/weapons/${id}`);
   };
-  console.log(weapons);
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        overflowY: 'scroll',
-        maxHeight: '78vh',
-      }}
-    >
-      {weapons.map((weapon) => {
-        return (
-          <div className="allWeaponsContainer" style={{ margin: '8px 4px' }}>
-            <div className={classes.square}>
-              <img src={weapon.imageUrl} className={classes.weaponImg} />
-              <div className="weaponName">{weapon.name}</div>
-              <div>
-                <Button
-                  style={{
-                    color: 'black',
-                    textAlign: 'center',
-                  }}
-                  onClick={() => handleClick(weapon.id)}
-                  class="button"
-                >
-                  View Weapon
-                </Button>
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <input
+          placeholder="Enter Weapon Here"
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          overflowY: "auto",
+          maxHeight: "78vh",
+          minHeight: "78vh",
+        }}
+      >
+        {weapons
+          .filter((weapon) => {
+            if (search === "") {
+              //if the search is empty
+              return weapon;
+            } else if (
+              weapon.name.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return weapon;
+            }
+          })
+          .map((weapon) => {
+            return (
+              <div
+                className="allWeaponsContainer"
+                style={{ margin: "8px 4px" }}
+              >
+                <div className={classes.square}>
+                  <img src={weapon.imageUrl} className={classes.weaponImg} />
+                  <div className="weaponName">{weapon.name}</div>
+                  <div>
+                    <Button
+                      style={{
+                        color: "black",
+                        textAlign: "center",
+                      }}
+                      onClick={() => handleClick(weapon.id)}
+                      class="button"
+                    >
+                      View Weapon
+                    </Button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+            );
+          })}
+      </div>
+    </>
   );
 };
 export default AllWeapons;
